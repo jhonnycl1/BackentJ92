50874e1cba1b03b76c34dbffddd6b011d2a0d920
jhonnycl1 <102738966+jhonnycl1@users.noreply.github.com>
2022-12-17 03:15:48 -0500

    Update proyectomgController.java

--- src/main/java/com/example/demoMongoDB/Controller/proyectomgController.java
+++ src/main/java/com/example/demoMongoDB/Controller/proyectomgController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@CrossOrigin(origins = "https://jhonnyfrontcertus.azurewebsites.net/")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/proyectomgBean")
 public class proyectomgController {
