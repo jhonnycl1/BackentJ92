dade0a57f2c11fb98feab1103df354758e82c234
jhonnycl1 <102738966+jhonnycl1@users.noreply.github.com>
2022-12-17 03:16:16 -0500

    Update proyectomgRepository.java

--- src/main/java/com/example/demoMongoDB/Repository/proyectomgRepository.java
+++ src/main/java/com/example/demoMongoDB/Repository/proyectomgRepository.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
 import java.util.List;
 
 
-@CrossOrigin(origins = "https://jhonnyfrontcertus.azurewebsites.net/")
+@CrossOrigin(origins = "http://localhost:4200/")
 //@RepositoryRestResource(collectionResourceRel = "proyectomgBean", path="proyectomgBean")
 public interface proyectomgRepository extends MongoRepository<proyectomgBean, String>{
 }
